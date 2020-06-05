b432eec7dd1db74665f0aa03225169b9a9758b72
xueyin220807 <1509272975@qq.com>
2020-06-05 23:06:13 +0800

    Update index.md
    C:/Users/xy/project/pwa3/dist/js
    C:/Users/xy/project/pwa3/dist/favicon.ico
    C:/Users/xy/project/pwa3/dist/index.html
    C:/Users/xy/project/pwa3/dist/manifest.json
    C:/Users/xy/project/pwa3/dist/precache-manifest.e8e73119b8fa65dc25182e8e58562d0c.js
    C:/Users/xy/project/pwa3/dist/robots.txt
    C:/Users/xy/project/pwa3/dist/service-worker.js
    C:/Users/xy/project/pwa3/dist/css
    C:/Users/xy/project/pwa3/dist/img
    
    add version
    
    20200806
    
    20200807
    
    20200808
    
    20200809

---
+++ precache-manifest.e8e73119b8fa65dc25182e8e58562d0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e439b6fd95bcb9e727",
+    "url": "/css/app.1c17b6db.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "610f55be71858cd545c8eca07b5b58f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6e439b6fd95bcb9e727",
+    "url": "/js/app.f73030f9.js"
+  },
+  {
+    "revision": "2c89fc4604ebfa68cbdb",
+    "url": "/js/chunk-vendors.0396140b.js"
+  },
+  {
+    "revision": "6218073d14e41bf73dca3629a041535c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
